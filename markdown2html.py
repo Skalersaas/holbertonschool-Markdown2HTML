4e8b08f5c5f1b264df529cb17a229da2230a4a7d
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-02-09 19:00:14 +0400

    Update markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -7,23 +7,23 @@ Second argument is the output file name """
 import sys
 import os
 def Heading(line):
-    count = line.count('#')
-    return "<H"+str(count)+">"+line[count:]+"</H"+str(count)+">"
-
+    count = line.count("#")
+    helper = "<H"+count+">"
+    return helper+line[count:]+helper.replace("<","</")
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        text = open(sys.argv[0],"r")
-        lines = text.splitlines()
-        HTML = ""
-        
-        for line in lines:
-            if(line[0]=='#'):
-                HTML += Heading(line)
-
-        htmlfile = open(sys.argv[1],"w")
-        htmlfile.write(HTML)
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         exit(1)
     if not os.path.exists(sys.argv[1]):
         sys.stderr.write("Missing " + sys.argv[1] + "\n")
         exit(1)
+
+
+    with open(sys.argv[1]) as MD:
+        with open(sys.argv[2],'w') as HTML:
+            html = ""
+            for line in MD:
+                if(line[0]=="#"):
+                    html+=Heading(line)
+            HTML.write(html)
     exit(0)
