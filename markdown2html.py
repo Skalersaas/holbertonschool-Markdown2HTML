200acf96b777716c0392670a7d32324507d04838
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-02-09 18:29:38 +0400

    Update markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -8,8 +8,9 @@ import sys
 import os
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         exit(1)
     if not os.path.exists(sys.argv[1]):
         sys.stderr.write("Missing " + sys.argv[1] + "\n")
         exit(1)
+    exit(0)
